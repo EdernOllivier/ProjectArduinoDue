df77639f2c436dde3104dd2f6797ad24926b1fe2
Edern Ollivier <53602402+EdernOllivier@users.noreply.github.com>
2021-11-02 16:27:05 +0100

    Add files via upload
    The fixing has been done on the Due board with the mounted gyrometer and also the orientation sensor by the shield of the motor.

--- Main.ino
+++ Main.ino
@@ -1,19 +1,20 @@
-/* 
-This is a test sketch for the Adafruit assembled Motor Shield for Arduino v2
-It won't work with v1.x motor shields! Only for the v2's with built in PWM
-control
+/*
+  This is a test sketch for the Adafruit assembled Motor Shield for Arduino v2
+  It won't work with v1.x motor shields! Only for the v2's with built in PWM
+  control
 
-For use with the Adafruit Motor Shield v2 
----->	http://www.adafruit.com/products/1438
+  For use with the Adafruit Motor Shield v2
+  ---->	http://www.adafruit.com/products/1438
 */
-
+//#include "I2CScanner.h"
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
-
+#define Wire Wire1
+//I2CScanner scanner;
 // Create the motor shield object with the default I2C address
-Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 // Or, create it with a different I2C address (say for stacking)
-// Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61); 
+// Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61);
 
 // Select which 'port' M1, M2, M3 or M4. In this case, M1
 Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
@@ -22,12 +23,13 @@ Adafruit_DCMotor *myOtherMotor = AFMS.getMotor(2);
 // You can also make another stepper motor on port M3 and M4
 Adafruit_StepperMotor *myNextMotor = AFMS.getStepper(200, 2);
 
-int sensorPin1 = A0;    // select the input pin for the gyrometer
-int sensorValue1 = 0;  // variable to store the value coming from the sensor
-
-int sensorPin2 = A1;    // select the input pin for the inclinometer
-int sensorValue2 = 0;  // variable to store the value coming from the sensor
-
+int sensorPin1 = A1;    // select the input pin for the gyrometer // gyrometer wiper (middle terminal) connected to analog pin 1
+// outside leads to ground and +3.3V
+int sensorValue1 = 0;   // variable to store the value coming from the sensor
+/*
+  int sensorPin2 = A1;    // select the input pin for the inclinometer
+  int sensorValue2 = 0;  // variable to store the value coming from the sensor
+*/
 //#include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
@@ -143,49 +145,49 @@ void displayCalStatus(void)
   Serial.print(mag, DEC);
 }
 
-#include <QuadratureEncoder.h>
+//#include <QuadratureEncoder.h>
 // must also have enableInterrupt.h library
 //#include <avr/interrupt.h>
 
-// Use any 2 pins for interrupt, this utilizes EnableInterrupt Library. 
+// Use any 2 pins for interrupt, this utilizes EnableInterrupt Library.
 // Even analog pins can be used. A0 = 14,A1=15,..etc for arduino nano/uno
 
 // Max number of Encoders object you can create is 4. This example only uses 2.
 
 // digital pins 2, 4 have an encoder attached to it. Give it a name:
-int EncoderA = 2;
-int EncoderB = 4;
+//int EncoderA = 2;
+//int EncoderB = 4;
 
-Encoders leftEncoder(EncoderA, EncoderB);  // Create an Encoder object name leftEncoder, using digitalpin 2 & 4
-//Encoders rightEncoder(15,14); // Encoder object name rightEncoder using analog pin A0 and A1 
+//Encoders leftEncoder(EncoderA, EncoderB);  // Create an Encoder object name leftEncoder, using digitalpin 2 & 4
+//Encoders rightEncoder(15,14); // Encoder object name rightEncoder using analog pin A0 and A1
 
 #include <Wire.h>
 #include <SPI.h>
-#include <Adafruit_LSM9DS1.h>
-#include <Adafruit_Sensor.h>  // not used in this demo but required!
+//#include <Adafruit_LSM9DS1.h>
+//#include <Adafruit_Sensor.h>  // not used in this demo but required!
+/*
+  // i2c
+  Adafruit_LSM9DS1 lsm = Adafruit_LSM9DS1();
 
-// i2c
-Adafruit_LSM9DS1 lsm = Adafruit_LSM9DS1();
-
-#define LSM9DS1_SCK A5
-#define LSM9DS1_MISO 12
-#define LSM9DS1_MOSI A4
-#define LSM9DS1_XGCS 6
-#define LSM9DS1_MCS 5
-// You can also use software SPI
-//Adafruit_LSM9DS1 lsm = Adafruit_LSM9DS1(LSM9DS1_SCK, LSM9DS1_MISO, LSM9DS1_MOSI, LSM9DS1_XGCS, LSM9DS1_MCS);
-// Or hardware SPI! In this case, only CS pins are passed in
-//Adafruit_LSM9DS1 lsm = Adafruit_LSM9DS1(LSM9DS1_XGCS, LSM9DS1_MCS);
-
-
-void setupSensor()
-{
+  #define LSM9DS1_SCK A5
+  #define LSM9DS1_MISO 12
+  #define LSM9DS1_MOSI A4
+  #define LSM9DS1_XGCS 6
+  #define LSM9DS1_MCS 5
+  // You can also use software SPI
+  //Adafruit_LSM9DS1 lsm = Adafruit_LSM9DS1(LSM9DS1_SCK, LSM9DS1_MISO, LSM9DS1_MOSI, LSM9DS1_XGCS, LSM9DS1_MCS);
+  // Or hardware SPI! In this case, only CS pins are passed in
+  //Adafruit_LSM9DS1 lsm = Adafruit_LSM9DS1(LSM9DS1_XGCS, LSM9DS1_MCS);
+*/
+/*
+  void setupSensor()
+  {
   // 1.) Set the accelerometer range
   lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_2G);
   //lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_4G);
   //lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_8G);
   //lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_16G);
-  
+
   // 2.) Set the magnetometer sensitivity
   lsm.setupMag(lsm.LSM9DS1_MAGGAIN_4GAUSS);
   //lsm.setupMag(lsm.LSM9DS1_MAGGAIN_8GAUSS);
@@ -196,15 +198,15 @@ void setupSensor()
   lsm.setupGyro(lsm.LSM9DS1_GYROSCALE_245DPS);
   //lsm.setupGyro(lsm.LSM9DS1_GYROSCALE_500DPS);
   //lsm.setupGyro(lsm.LSM9DS1_GYROSCALE_2000DPS);
-}
-
+  }
+*/
 void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
   Serial.println("Adafruit Motorshield v2 - DC Motor test!");
 
   AFMS.begin();  // create with the default frequency 1.6KHz
   //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
-  
+
   // Set the speed to start, from 0 (off) to 255 (max speed)
   myMotor->setSpeed(150);
   myMotor->run(FORWARD);
@@ -219,20 +221,22 @@ void setup() {
 
   Serial.println("Stepper test!");
 
-//  AFMS.begin();  // create with the default frequency 1.6KHz
+  //  AFMS.begin();  // create with the default frequency 1.6KHz
   //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
-  
-  myNextMotor->setSpeed(10);  // 10 rpm   
 
-//  Serial.begin(115200);
+  myNextMotor->setSpeed(10);  // 10 rpm
+
+  //  Serial.begin(115200);
   Serial.println("Orientation Sensor Test"); Serial.println("");
 
+  //  scanner.Init();
   /* Initialise the sensor */
-  if(!bno.begin())
+
+  if (!bno.begin())
   {
     /* There was a problem detecting the BNO055 ... check your connections */
     Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
-    while(1);
+    while (1);
   }
 
   delay(1000);
@@ -244,34 +248,53 @@ void setup() {
   displaySensorStatus();
 
   bno.setExtCrystalUse(true);
+  /*
+    //////////////////////////////////////////////
+    // make the pushbutton's pin an input:
+    pinMode(EncoderA, INPUT);
+    pinMode(EncoderB, INPUT);
 
-//////////////////////////////////////////////
-  // make the pushbutton's pin an input:
-  pinMode(EncoderA, INPUT);
-  pinMode(EncoderB, INPUT);
+    while (!Serial) {
+      delay(1); // will pause Zero, Leonardo, etc until serial console opens
+    }
 
-  while (!Serial) {
-    delay(1); // will pause Zero, Leonardo, etc until serial console opens
-  }
-  
-  Serial.println("LSM9DS1 data read demo");
-  
-  // Try to initialise and warn if we couldn't detect the chip
-  if (!lsm.begin())
-  {
-    Serial.println("Oops ... unable to initialize the LSM9DS1. Check your wiring!");
-    while (1);
-  }
-  Serial.println("Found LSM9DS1 9DOF");
+    Serial.println("LSM9DS1 data read demo");
 
-  // helper to just set the default scaling we want, see above!
-  setupSensor();
+    // Try to initialise and warn if we couldn't detect the chip
+    if (!lsm.begin())
+    {
+      Serial.println("Oops ... unable to initialize the LSM9DS1. Check your wiring!");
+      while (1);
+    }
+    Serial.println("Found LSM9DS1 9DOF");
+
+    // helper to just set the default scaling we want, see above!
+    setupSensor();
+  */
 }
 
 unsigned long lastMilli = 0;
 
 void loop() {
-  /* Get a new sensor event */
+  // read the value from the gyrometer sensor:
+  sensorValue1 = analogRead(sensorPin1); // - 512;
+  Serial.println("From the gyrometer : ");
+  Serial.print(sensorValue1, DEC);
+  Serial.println("");
+  //  scanner.Scan();
+
+  // a little delay to not hog Serial Monitor
+  delay(100);
+
+  // change the resolution to 12 bits and read A1
+  //  analogReadResolution(12);
+  //  Serial.print(", 12-bit : ");
+  //  Serial.print(analogRead(A1));/* Get a new sensor event */
+  //  Serial.println("");
+
+  // a little delay to not hog Serial Monitor
+  //  delay(100);
+
   sensors_event_t event;
   bno.getEvent(&event);
 
@@ -287,191 +310,198 @@ void loop() {
   displayCalStatus();
 
   /* Optional: Display sensor status (debug only) */
-  //displaySensorStatus();
+  displaySensorStatus();
 
   /* New line for the next sample */
   Serial.println("");
 
   /* Wait the specified delay before requesting nex data */
   delay(BNO055_SAMPLERATE_DELAY_MS);
-//////////////////////////////////////////////////////////////////////////////////  
-  // put your main code here, to run repeatedly:
-  // print encoder count every 50 millisecond
-  if(millis()-lastMilli > 50){ 
-    
-    long currentLeftEncoderCount = leftEncoder.getEncoderCount();
-//    long currentRightEncoderCount = rightEncoder.getEncoderCount();
-    
-    // read the value from the odometer sensor:
-    Serial.println("From the odometer : ");
-    Serial.print(currentLeftEncoderCount);
-//    Serial.print(" , ");
-//    Serial.println(currentRightEncoderCount);
-    Serial.println("");
-    
-    lastMilli = millis();
-  }
-//////////////////////////////////////////////////////////////////////////////////   
-  uint8_t i;
-  
+  /*
+    ///////////////////////////////////////////////////////////////////////////////////
+    // put your main code here, to run repeatedly:
+    // print encoder count every 50 millisecond
+    if(millis()-lastMilli > 50){
+
+      long currentLeftEncoderCount = leftEncoder.getEncoderCount();
+    //    long currentRightEncoderCount = rightEncoder.getEncoderCount();
+
+      // read the value from the odometer sensor:
+      Serial.println("From the odometer : ");
+      Serial.print(currentLeftEncoderCount);
+    //    Serial.print(" , ");
+    //    Serial.println(currentRightEncoderCount);
+      Serial.println("");
+
+      lastMilli = millis();
+    }
+    //////////////////////////////////////////////////////////////////////////////////
+  */
+
+  //uint8_t i;
+
   // read the value from the gyrometer sensor:
-  sensorValue1 = analogRead(sensorPin1) -512;
+  sensorValue1 = analogRead(sensorPin1); // - 512;
   Serial.println("From the gyrometer : ");
   Serial.print(sensorValue1, DEC);
   Serial.println("");
+  /*
+    // read the value from the inclinometer sensor:
+    sensorValue2 = analogRead(sensorPin2);
+    Serial.println("From the inclinometer : ");
+    Serial.print(sensorValue2, DEC);
+    Serial.println("");
+  */
+  //  Serial.print("tick");
 
-  // read the value from the inclinometer sensor:
-  sensorValue2 = analogRead(sensorPin2);
-  Serial.println("From the inclinometer : ");
-  Serial.print(sensorValue2, DEC);
-  Serial.println("");
+  //  uint8_t i;
+  /*
+    Serial.print("tick");
 
-//  Serial.print("tick");
-
-//  uint8_t i;
-
-  Serial.print("tick");
-
-  myOtherMotor->run(FORWARD);
-  for (i=0; i<200; i++) {
-    myOtherMotor->setSpeed(i);  
-    delay(10);
-  }
-  for (i=200; i!=0; i--) {
-    myOtherMotor->setSpeed(i);  
-    delay(10);
-  }
-  
-  Serial.print("tock");
-
-  myOtherMotor->run(BACKWARD);
-  for (i=0; i<200; i++) {
-    myOtherMotor->setSpeed(i);  
-    delay(10);
-  }
-  for (i=200; i!=0; i--) {
-    myOtherMotor->setSpeed(i);  
-    delay(10);
-  }
-
-  Serial.print("tech");
-  myOtherMotor->run(RELEASE);
-  delay(1000);
-
-//////////////////////////////////////////////////////////////////////////////////////////
-  lsm.read();  /* ask it to read in the data */ 
-
-  /* Get a new sensor event */ 
-  sensors_event_t a, m, g, temp;
-
-  lsm.getEvent(&a, &m, &g, &temp); 
-
-  Serial.print("Accel X: "); Serial.print(a.acceleration.x); Serial.print(" m/s^2");
-  Serial.print("\tY: "); Serial.print(a.acceleration.y);     Serial.print(" m/s^2 ");
-  Serial.print("\tZ: "); Serial.print(a.acceleration.z);     Serial.println(" m/s^2 ");
-
-  Serial.print("Mag X: "); Serial.print(m.magnetic.x);   Serial.print(" uT");
-  Serial.print("\tY: "); Serial.print(m.magnetic.y);     Serial.print(" uT");
-  Serial.print("\tZ: "); Serial.print(m.magnetic.z);     Serial.println(" uT");
-
-  Serial.print("Gyro X: "); Serial.print(g.gyro.x);   Serial.print(" rad/s");
-  Serial.print("\tY: "); Serial.print(g.gyro.y);      Serial.print(" rad/s");
-  Serial.print("\tZ: "); Serial.print(g.gyro.z);      Serial.println(" rad/s");
-
-  Serial.println();
-//  delay(200);
-////////////////////////////////////////////////////////////////////////////////////////////
-//#ifdef TRUE
-// here the stab. has been done with the inclinometer 
-/*  
-if (event.orientation.y > 0)
-  { 
-    myMotor->run(BACKWARD);
-    myMotor->setSpeed(event.orientation.y *2);  
-//    myNextMotor->step(4, FORWARD, MICROSTEP); 
-  }
-  else 
-  {
-    myMotor->run(FORWARD);
-    myMotor->setSpeed(-1 *event.orientation.y *2);   
-//    myNextMotor->step(4, BACKWARD, MICROSTEP); 
-  } 
-
-// here the stab. has been done with the inclinometer
-  if (event.orientation.z > 0)
-  { 
-    myOtherMotor->run(BACKWARD);
-    myOtherMotor->setSpeed(event.orientation.z *2);  
-//    myNextMotor->step(4, FORWARD, MICROSTEP); 
-  }
-  else 
-  {
     myOtherMotor->run(FORWARD);
-    myOtherMotor->setSpeed(-1 *event.orientation.z *2);   
-//    myNextMotor->step(4, BACKWARD, MICROSTEP); 
-  } 
-*/
-//#else
-/*
-// here the stab. has been done with the gyrometer
-  if (sensorValue1 > 0)
-  { 
+    for (i=0; i<200; i++) {
+      myOtherMotor->setSpeed(i);
+      delay(10);
+    }
+    for (i=200; i!=0; i--) {
+      myOtherMotor->setSpeed(i);
+      delay(10);
+    }
+
+    Serial.print("tock");
+
+    myOtherMotor->run(BACKWARD);
+    for (i=0; i<200; i++) {
+      myOtherMotor->setSpeed(i);
+      delay(10);
+    }
+    for (i=200; i!=0; i--) {
+      myOtherMotor->setSpeed(i);
+      delay(10);
+    }
+
+    Serial.print("tech");
+    myOtherMotor->run(RELEASE);
+    delay(1000);
+
+    //////////////////////////////////////////////////////////////////////////////////////////
+  */
+
+  //  lsm.read();  /* ask it to read in the data */
+
+  /* Get a new sensor event */
+  /*
+    sensors_event_t a, m, g, temp;
+
+    lsm.getEvent(&a, &m, &g, &temp);
+
+    Serial.print("Accel X: "); Serial.print(a.acceleration.x); Serial.print(" m/s^2");
+    Serial.print("\tY: "); Serial.print(a.acceleration.y);     Serial.print(" m/s^2 ");
+    Serial.print("\tZ: "); Serial.print(a.acceleration.z);     Serial.println(" m/s^2 ");
+
+    Serial.print("Mag X: "); Serial.print(m.magnetic.x);   Serial.print(" uT");
+    Serial.print("\tY: "); Serial.print(m.magnetic.y);     Serial.print(" uT");
+    Serial.print("\tZ: "); Serial.print(m.magnetic.z);     Serial.println(" uT");
+
+    Serial.print("Gyro X: "); Serial.print(g.gyro.x);   Serial.print(" rad/s");
+    Serial.print("\tY: "); Serial.print(g.gyro.y);      Serial.print(" rad/s");
+    Serial.print("\tZ: "); Serial.print(g.gyro.z);      Serial.println(" rad/s");
+
+    Serial.println();
+  */
+  //  delay(200);
+  ////////////////////////////////////////////////////////////////////////////////////////////
+  //#ifdef TRUE
+  // here the stab. has been done with the inclinometer
+  /*
+    if (event.orientation.y > 0)
+    {
+      myMotor->run(BACKWARD);
+      myMotor->setSpeed(event.orientation.y *2);
+    //    myNextMotor->step(4, FORWARD, MICROSTEP);
+    }
+    else
+    {
+      myMotor->run(FORWARD);
+      myMotor->setSpeed(-1 *event.orientation.y *2);
+    //    myNextMotor->step(4, BACKWARD, MICROSTEP);
+    }
+
+    // here the stab. has been done with the inclinometer
+    if (event.orientation.z > 0)
+    {
+      myOtherMotor->run(BACKWARD);
+      myOtherMotor->setSpeed(event.orientation.z *2);
+    //    myNextMotor->step(4, FORWARD, MICROSTEP);
+    }
+    else
+    {
+      myOtherMotor->run(FORWARD);
+      myOtherMotor->setSpeed(-1 *event.orientation.z *2);
+    //    myNextMotor->step(4, BACKWARD, MICROSTEP);
+    }
+  */
+  //#else
+  /*
+    // here the stab. has been done with the gyrometer
+    if (sensorValue1 > 0)
+    {
+      myMotor->run(FORWARD);
+      myMotor->setSpeed(sensorValue1);
+    }
+    else
+    {
+      myMotor->run(BACKWARD);
+      myMotor->setSpeed(-1 *sensorValue1);
+    }
+    #endif
+  */
+  //#ifdef TRUE
+  /*
+    Serial.println("Single coil steps");
+    myNextMotor->step(100, FORWARD, SINGLE);
+    myNextMotor->step(100, BACKWARD, SINGLE);
+
+    Serial.println("Double coil steps");
+    myNextMotor->step(100, FORWARD, DOUBLE);
+    myNextMotor->step(100, BACKWARD, DOUBLE);
+
+    Serial.println("Interleave coil steps");
+    myNextMotor->step(100, FORWARD, INTERLEAVE);
+    myNextMotor->step(100, BACKWARD, INTERLEAVE);
+
+    Serial.println("Microstep steps");
+    myNextMotor->step(50, FORWARD, MICROSTEP);
+    myNextMotor->step(50, BACKWARD, MICROSTEP);
+  */
+  /*
+    // for the stepper motor to be actionned
+    //#else
+    //  Serial.print("tick");
     myMotor->run(FORWARD);
-    myMotor->setSpeed(sensorValue1);  
-  }
-  else 
-  {
+    for (i=0; i<50; i++) {
+      myMotor->setSpeed(i);
+      delay(10);
+    }
+    for (i=50; i!=0; i--) {
+      myMotor->setSpeed(i);
+      delay(10);
+    }
+
+    //  Serial.print("tock");
     myMotor->run(BACKWARD);
-    myMotor->setSpeed(-1 *sensorValue1);   
-  } 
-#endif
-*/
-//#ifdef TRUE
-/*
-  Serial.println("Single coil steps");
-  myNextMotor->step(100, FORWARD, SINGLE); 
-  myNextMotor->step(100, BACKWARD, SINGLE); 
+    for (i=0; i<50; i++) {
+      myMotor->setSpeed(i);
+      delay(10);
+    }
+    for (i=50; i!=0; i--) {
+      myMotor->setSpeed(i);
+      delay(10);
+    }
 
-  Serial.println("Double coil steps");
-  myNextMotor->step(100, FORWARD, DOUBLE); 
-  myNextMotor->step(100, BACKWARD, DOUBLE);
-  
-  Serial.println("Interleave coil steps");
-  myNextMotor->step(100, FORWARD, INTERLEAVE); 
-  myNextMotor->step(100, BACKWARD, INTERLEAVE); 
-
-  Serial.println("Microstep steps");
-  myNextMotor->step(50, FORWARD, MICROSTEP); 
-  myNextMotor->step(50, BACKWARD, MICROSTEP);
-*/
-/*
-// for the stepper motor to be actionned
-//#else
-//  Serial.print("tick");
-  myMotor->run(FORWARD);
-  for (i=0; i<50; i++) {
-    myMotor->setSpeed(i);  
-    delay(10);
-  }
-  for (i=50; i!=0; i--) {
-    myMotor->setSpeed(i);  
-    delay(10);
-  }
-
-//  Serial.print("tock");
-  myMotor->run(BACKWARD);
-  for (i=0; i<50; i++) {
-    myMotor->setSpeed(i);  
-    delay(10);
-  }
-  for (i=50; i!=0; i--) {
-    myMotor->setSpeed(i);  
-    delay(10);
-  }
-
-//  Serial.print("tech");
-  myMotor->run(RELEASE);
-  delay(1000);
-//#endif
-*/
+    //  Serial.print("tech");
+    myMotor->run(RELEASE);
+    delay(1000);
+    //#endif
+  */
 }
